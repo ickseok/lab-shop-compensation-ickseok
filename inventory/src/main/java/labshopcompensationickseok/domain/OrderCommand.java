f4baa7b3c21e20e8b7967575a4914ad121071ca1
ickseok <67779665+ickseok@users.noreply.github.com>
2022-09-27 10:10:11 +0900

    commit

---
+++ inventory/src/main/java/labshopcompensationickseok/domain/OrderCommand.java
@@ -0,0 +1,13 @@
+package labshopcompensationickseok.domain;
+
+import javax.persistence.*;
+import java.util.List;
+import java.util.Date;
+import lombok.Data;
+
+@Data
+public class OrderCommand {
+
+
+
+}
---
+++ order/src/main/java/labshopcompensationickseok/infra/PolicyHandler.java
@@ -0,0 +1,28 @@
+package labshopcompensationickseok.infra;
+
+import javax.naming.NameParser;
+
+import javax.naming.NameParser;
+import javax.transaction.Transactional;
+
+import labshopcompensationickseok.config.kafka.KafkaProcessor;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.stream.annotation.StreamListener;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.stereotype.Service;
+import labshopcompensationickseok.domain.*;
+
+
+@Service
+@Transactional
+public class PolicyHandler{
+    @Autowired OrderRepository orderRepository;
+    
+    @StreamListener(KafkaProcessor.INPUT)
+    public void whatever(@Payload String eventString){}
+
+}
+
+
